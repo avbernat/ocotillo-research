f16dc50d829f035f30c133b620d683d7baab927b
avbernat <57693577+avbernat@users.noreply.github.com>
2022-12-20 19:22:52 -0600

    PCA without IQR variables regressions.

--- src/pca.R
+++ src/pca.R
@@ -9,21 +9,16 @@ Round <- function(number){
   }
 }
 
-PCA_graphs <- function(dataset, PCA_title, abbrev_names, run_corr=FALSE){
+PCA_graphs <- function(dataset, PCA_title, run_corr=FALSE){
   
   # cos2 and the alpha.var: alpha.var colours variables by cos2 
   # (importance of most important PC to variable), 
   # see https://personal.utdallas.edu/~herve/abdi-awPCA2010.pdf
   
   GFpca <- PCA(dataset, scale.unit = TRUE, graph = FALSE, ncp = 10)
-  
   eig.val <- get_eigenvalue(GFpca)
   var.val <- GFpca$var
-  # print(eig.val) #will only show in in console
-  # print(var.val)
-  
   scree <- fviz_eig(GFpca, addlabels = TRUE, ylim = c(0, 50))
-  # print(scree)
   
   labX <- paste("PC1 (", Round(eig.val[1, 2]), "%)", sep = "")
   labY <- paste("PC1 (", Round(eig.val[2, 2]), "%)", sep = "")
@@ -40,11 +35,28 @@ PCA_graphs <- function(dataset, PCA_title, abbrev_names, run_corr=FALSE){
   )
   
   if (run_corr) {
-    D = cor(d)
-    test <- cor.mtest(d)$p
-    corrplot.mixed(D,lower.col = "black", number.cex = .7, p.mat=test, sig.level=0.05)
-    corrplot.mixed(D,lower.col = "black", number.cex = .7)
+    corr_dataset = cor(dataset)
+    pvalues <- cor.mtest(dataset)$p
+    cor_graph_sig = corrplot.mixed(corr_dataset, number.cex = .7, p.mat=pvalues, sig.level=0.05)
+    
+    corrplot(var.val$cos2, number.cex = .7, is.corr=FALSE)
+    corrplot(var.val$contrib, number.cex = .7, is.corr=FALSE)
   }
   
-  return(pca)
+  pcaResults <- as.data.frame(GFpca$ind$coord)
+  
+  output_list = list(eig.val, var.val, scree, GFpca, pca, pcaResults)
+  
+  return(output_list)
+}
+
+run_corrplots = function(dataset, pca_vars) {
+  
+  corr_dataset = cor(dataset)
+  pvalues <- cor.mtest(dataset)$p
+  corrplot.mixed(corr_dataset, number.cex = .7, p.mat=pvalues, sig.level=0.05, 
+                                        title="significant correlations", mar=c(0,0,1,0))
+  
+  corrplot(pca_vars$cos2, number.cex = .7, is.corr=FALSE, title="cos2 (variable representation)", mar=c(0,0,1,0))
+  corrplot(pca_vars$contrib, number.cex = .7, is.corr=FALSE, title="contribution", mar=c(0,0,1,0))
 }
